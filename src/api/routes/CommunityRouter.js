cfe163431a971e430ae76492fd2ec64f9a7e9370
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-11-17 20:40:21 +0200

    Added route for post creation

--- src/api/routes/CommunityRouter.js
+++ src/api/routes/CommunityRouter.js
@@ -5,10 +5,11 @@ import verifyToken from "../middleware/verifyToken.js";
 const CommunityRouter = new Router();
 
 CommunityRouter
-.post(("/create"), verifyToken, CommunityController.create)
-.post(("/:id/edit"), verifyToken, CommunityController.edit)
-.post(("/:id/delete"), verifyToken, CommunityController.delete)
-.post(("/:id/add-member"), verifyToken, CommunityController.addMember)
-.post(("/:id/remove-member"), verifyToken, CommunityController.removeMember)
+.post("/create", verifyToken, CommunityController.create)
+.post("/:id/edit", verifyToken, CommunityController.edit)
+.post("/:id/delete", verifyToken, CommunityController.delete)
+.post("/:id/add-member", verifyToken, CommunityController.addMember)
+.post("/:id/remove-member", verifyToken, CommunityController.removeMember)
+.post("/:id/create-post", verifyToken, CommunityController.createPost)
 
 export default CommunityRouter;
