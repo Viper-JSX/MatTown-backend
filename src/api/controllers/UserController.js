ed64e98815c5a3565631d20dea442f2a21c071af
Yurii Shtefanko <yuramcpecs@gmail.com>
2024-02-13 22:10:20 +0200

    Implemented user id validation before fetching user

--- src/api/controllers/UserController.js
+++ src/api/controllers/UserController.js
@@ -1,5 +1,17 @@
+import mongoose from "mongoose";
+import UserModel from "../models/UserModel.js";
+
 class UserController {
     getUser (req, res) {
+        const userId = req.params.id;
+
+        if (!mongoose.Types.ObjectId.isValid(userId)) {
+            console.log("Invalid user id");
+            return res.status(403).json({ message: "Invalid user id provided" });
+        }
+
+        const user = UserModel.findById(userId);
+        console.log("getting user", userId)
         res.json({ message: "Getting user", id: req.params.id });
     }
 
